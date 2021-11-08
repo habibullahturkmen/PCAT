ffba7f1e353aa78ccefb3ad3831ea081d1195e65
habibullahturkmen <habibullah_turkmen@yahoo.con>
2021-11-08 20:33:30 +0430

    Ready for Deployment

---
+++ app.js
@@ -0,0 +1,50 @@
+// Third party modules
+const express = require('express');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
+const ejs = require('ejs');
+// controller modules
+const photoController = require('./controllers/photoController');
+const pageController = require('./controllers/pageController');
+
+// initialize express
+const app = express();
+
+// Connect DB
+mongoose.connect('mongodb+srv://habibullah:hw28ub4yzoTYWpQ6@cluster0.gtkue.mongodb.net/pcat-db?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('DB Connected');
+}).catch((error) => {
+    console.log(error);
+});
+
+// Template Engine
+app.set('view engine', 'ejs');
+
+// Execute Middlewares
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(fileUpload());
+app.use(methodOverride('_method', {
+    methods: ['POST', 'GET']
+}));
+
+// Routes: photoController
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
+// Routes: pageController
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server started at port ${port}`);
+});
